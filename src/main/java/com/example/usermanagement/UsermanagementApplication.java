d1f2cd0e28523f0d1105ef5d340a99b22003e2a9
USER <nikhithajaisy@gmail.com>
2024-10-12 17:10:09 +0530

    chnahes in main

--- src/main/java/com/example/usermanagement/UsermanagementApplication.java
+++ src/main/java/com/example/usermanagement/UsermanagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UsermanagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("main branch");
 		SpringApplication.run(UsermanagementApplication.class, args);
 	}
 
